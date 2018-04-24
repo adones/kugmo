a142271e89ae915adb7554e2ac383dc31df509ff
Don Fuentes <adones.fuentes@gmail.com>
2018-04-24 15:31:52 +0400

    Initial Commit

---
+++ Kugmo/Kugmo.h
@@ -0,0 +1,19 @@
+//
+//  Kugmo.h
+//  Kugmo
+//
+//  Created by Adones Fuentes on 4/24/18.
+//  Copyright © 2018 Arabian Radio Network. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Kugmo.
+FOUNDATION_EXPORT double KugmoVersionNumber;
+
+//! Project version string for Kugmo.
+FOUNDATION_EXPORT const unsigned char KugmoVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Kugmo/PublicHeader.h>
+
+
